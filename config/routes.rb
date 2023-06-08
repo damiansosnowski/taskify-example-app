cbbc8618c9bda2e087a106bf5a3c0c2377d71c77
Damian Sosnowski <sosnowski.damian@gmail.com>
2023-06-08 09:45:25 +0200

    Route for toggle

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "tasks#index"
 
   resources :tasks
+  post "tasks/:id/toggle", to: "tasks#toggle"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
