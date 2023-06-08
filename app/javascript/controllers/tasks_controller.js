4b06c0e334fc9bb8aa7ddf41643f966e2ba53022
Damian Sosnowski <sosnowski.damian@gmail.com>
2023-06-08 09:41:54 +0200

    Toggle action in js controller

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -6,7 +6,24 @@ export default class extends Controller {
     console.log(this.element)
   }
 
-  toggle() {
-    console.log("toggle")
-  }
+  toggle(e) {
+    const id = e.target.dataset.id
+    const csrfToken = document.querySelector("[name='csrf-token']").content
+
+    fetch(`/tasks/${id}/toggle`, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': csrfToken
+        },
+        body: JSON.stringify({ completed: e.target.checked }) // body data type must match "Content-Type" header
+    })
+      .then(response => response.json())
+      .then(data => {
+         alert(data.message)
+       })
+}
 }
